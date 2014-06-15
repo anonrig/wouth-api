4724d2ead84988e76c1677c109ac00da0302738d
ynizipli <ynizipli@sabanciuniv.edu>
2014-06-15 21:57:18 +0300

    Add config.

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "app": {
+        "port": 4000,
+        "debug": false,
+        "jsonSpaces": 0
+    },
+    "db": {
+        "host": "mongodb://localhost/wouth"
+    },
+    "redis": {
+        "host": "localhost",
+        "port": 6379
+    }
+};
+
+
