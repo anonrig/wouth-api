77b50b75ec894681a95b344133f4612aff7d6c61
ynizipli <ynizipli@sabanciuniv.edu>
2014-06-15 21:57:26 +0300

    Add mongoose.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var express = require('express'),
     app = express(),
+    logger = require('morgan'),
+    mongoose = require('mongoose'),
     router = require('./router')(app),
-    logger = require('morgan');
+    config = require('./config');
 
 app.use(logger());
-app.use(express.static(__dirname + '/public'));;
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000);
+mongoose.connect(config.db.host);
+
+mongoose.connection.on('error', function (err) {
+    console.error('Mongo Connection ', err);
+});
+
+mongoose.connection.once('open', function callback() {
+    console.log('Mongo Connection OK.');
+    app.listen(config.app.port);
+});
+
+
+
--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+var models = require('./models');
+
 module.exports = function(app) {
   app.get('/about', function(req, res) {
       res.send('hello world');
