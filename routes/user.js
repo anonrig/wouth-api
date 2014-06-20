90d95f720728b3f746f953be5c73d81daac77506
ynizipli <ynizipli@sabanciuniv.edu>
2014-06-20 23:31:22 +0300

    Add login route.

--- routes/user.js
+++ routes/user.js
@@ -42,6 +42,19 @@ user.post('/', function(req, res) {
         });
 });
 
+user.post('/login', function(req, res) {
+     var username = req.body.username,
+         password = User.generateHash(req.body.password);
+
+    User.findOne({username: username}, function(err, user) {
+        if (user && user.password == password)
+            res.send(user);
+        else
+            res.send(500);
+    }, function(err) {
+        res.send(500);
+    });
+});
 
 user.get('/me', authenticatedMiddleware, function(req, res) {
    res.json(req.user);
